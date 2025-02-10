dd8d3cc13f6b2d70ca5e596d505237ae020ed9ea
masaud04 <ahmedmasaud942@gmail.com>
2025-02-10 19:51:17 +0600

    Implement custom cursor and enhance button styles with animations

---
+++ components/meta/MetaTags.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+export default function MetaTags() {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/meta/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/meta/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/meta/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/meta/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/meta/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#162447" />
+
+      <title>masaud ahmod</title>
+      <meta name="title" content="masaud ahmod" />
+      <meta
+        name="description"
+        content="Vivek's Portfolio Website with his past experiences, projects, and resume."
+      />
+      <meta name="author" content="masaud ahmod (masaudahmod)" />
+      <meta
+        name="keywords"
+        content="masaudahmod, masaudahmod portfolio, masaud ahmod protfolio,masaud ahmod computer science, masaud ahmod, masaud ahmod asu, masaud ahmod arizona state university, masaud ahmod basic portfolio"
+      />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://masaudahmod.com/" />
+      <meta property="og:title" content="masaud ahmod" />
+      <meta
+        property="og:description"
+        content="Vivek's Portfolio Website with his past experiences, projects, resume."
+      />
+      <meta property="og:image" content="/meta/meta-social-image.png" />
+      <meta name="og:locale" content="en_IN" />
+      <meta name="og:type" content="website" />
+
+      <meta name="twitter:site" content="masaudahmod" />
+      <meta name="twitter:creator" content="masaudahmod" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://masaudahmod.com/" />
+      <meta property="twitter:title" content="masaud ahmod" />
+      <meta
+        property="twitter:description"
+        content="Vivek's Portfolio Website with his past experiences, projects, resume."
+      />
+      <meta property="twitter:image" content="/meta/meta-social-image.png" />
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,25 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "masaud-dev-light-grey": "#d4cfcf",
+        "masaud-dev-dark-grey": "#182635",
+        "masaud-dev-purple": "#162447",
+        "masaud-dev-cyan": "#00FFFF",
+        "masaud-dev-teal": "#00ffdc",
+        "masaud-dev-dark-teal": "#00FFC2",
+        "masaud-dev-pink": "#FF55BB",
+        "masaud-dev-yellow": "#FEFF86",
+        "masaud-dev-green": "#30C85E",
+        "masaud-dev-blue": "#4A97FE",
+        "masaud-dev-primary-black": "#181818",
+        "masaud-dev-secondary-black": "#1E1E1F",
+        "ubuntu-orange": "#E95420",
       },
     },
   },
