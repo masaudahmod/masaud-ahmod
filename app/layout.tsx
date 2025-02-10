6d9f213a926d64651feeb7e4c0b4a420e828b031
masaud04 <ahmedmasaud942@gmail.com>
2025-02-10 21:03:23 +0600

    Add SVG assets, implement AppContext, and enhance layout with Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Cursor from "@/components/helper/Cursor";
+import Header from "@/components/Header/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header finishedLoading={true} sectionsRef={null}/>
         <Cursor />
         {children}
       </body>
---
+++ components/Header/Header.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useRef, useState, useEffect, useContext } from "react";
+import Logo from "./Headercomp/Logo";
+import DesktopMenu from "./Headercomp/DesktopMenu";
+import IconMenu from "./Headercomp/IconMenu";
+import MobileMenu from "./Headercomp/MobileMenu";
+import { motion } from "framer-motion";
+import AppContext from "../AppContextFolder/AppContext";
+
+const Header = ({ finishedLoading }: { finishedLoading: boolean }) => {
+  const RefNavBar = useRef<HTMLDivElement | null>(null);
+  const [ShowElement, setShowElement] = useState(false);
+  const [rotate, setRotate] = useState(false);
+  const context = useContext(AppContext);
+  const scrollSizeY = useRef(0);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 50) {
+  //       if (window.scrollY > scrollSizeY.current) {
+  //         RefNavBar.current?.classList.replace(
+  //           "translate-y-0",
+  //           "-translate-y-full"
+  //         );
+  //       } else {
+  //         RefNavBar.current?.classList.replace(
+  //           "-translate-y-full",
+  //           "translate-y-0"
+  //         );
+  //       }
+  //     }
+  //     scrollSizeY.current = window.scrollY;
+  //   };
+
+  //   if (!context.sharedState.portfolio.NavBar.scrolling) {
+  //     context.sharedState.portfolio.NavBar.scrolling = true;
+  //     window.addEventListener("scroll", handleScroll);
+  //   }
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, [context.sharedState.portfolio.NavBar]);
+
+  useEffect(() => {
+    setTimeout(() => setShowElement(true), 10400);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.overflow = rotate ? "hidden" : "auto";
+  }, [rotate]);
+
+  return (
+    <>
+      <MobileMenu
+        rotate={rotate}
+        setRotate={setRotate}
+        setShowElement={setShowElement}
+        ShowElement={ShowElement}
+      />
+      <motion.div
+        ref={RefNavBar}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          opacity: { delay: finishedLoading ? 0 : 9.4, duration: 0 },
+        }}
+        className={`w-full fixed ${
+          ShowElement ? `bg-opacity-70 shadow-xl` : `bg-opacity-0`
+        } bg-AAprimary flex justify-between px-6 sm:px-12 py-2 sm:py-4 transition duration-4000 translate-y-0 z-20`}
+      >
+        <Logo finishedLoading={finishedLoading} />
+        <IconMenu
+          rotate={rotate}
+          setRotate={setRotate}
+          setShowElement={setShowElement}
+          ShowElement={ShowElement}
+          finishedLoading={finishedLoading}
+        />
+        <DesktopMenu finishedLoading={finishedLoading} />
+      </motion.div>
+    </>
+  );
+};
+
+export default Header;
