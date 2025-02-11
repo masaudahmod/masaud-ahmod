928a8279dc8816b637ee58517ca2e5bbd95f5d7d
masaud04 <ahmedmasaud942@gmail.com>
2025-02-11 20:24:35 +0600

    Add GlowCard component and EducationSection; enhance styling with glow effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from "@/components/pages/About";
+import EducationSection from "@/components/pages/education-section/EducationSection";
 import HeroSection from "@/components/pages/hero-section/HeroSection";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HeroSection />
       <AboutMe />
+      <EducationSection />
     </>
   );
 }
