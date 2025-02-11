c3fc3667d1f84945bf7e476ffdc51adfed3ad3cb
masaud04 <ahmedmasaud942@gmail.com>
2025-02-11 12:11:46 +0600

    Refactor DesktopMenu component for improved readability and structure

--- components/Header/Headercomp/DesktopMenu.tsx
+++ components/Header/Headercomp/DesktopMenu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link as ReactScrollLink } from "react-scroll";
-
+// import ReactGA from "react-ga4";
 
 export default function DesktopMenu(props: { finishedLoading: boolean }) {
   return (
@@ -21,10 +21,18 @@ export default function DesktopMenu(props: { finishedLoading: boolean }) {
           delay: props.finishedLoading ? 0 : 9.4,
         }}
         className=" text-AAsecondary"
-       
       >
-        <ReactScrollLink to="aboutSection" spy={true} smooth={true} offset={-100} duration={200}>
-          &gt; 01. <span className="text-white hover:cursor-pointer hover:text-AAsecondary duration-300">About</span>
+        <ReactScrollLink
+          to="aboutSection"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={200}
+        >
+          &gt; 01.{" "}
+          <span className="text-white hover:cursor-pointer hover:text-AAsecondary duration-300">
+            About
+          </span>
         </ReactScrollLink>
       </motion.div>
       <motion.div
@@ -43,9 +51,17 @@ export default function DesktopMenu(props: { finishedLoading: boolean }) {
         }}
         className="text-AAsecondary"
       >
-        <ReactScrollLink to="WhereIhaveWorkedSection" spy={true} smooth={true} offset={-300} duration={200}>
+        <ReactScrollLink
+          to="WhereIhaveWorkedSection"
+          spy={true}
+          smooth={true}
+          offset={-300}
+          duration={200}
+        >
           &gt; 02.{" "}
-          <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Experience</span>
+          <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">
+            Experience
+          </span>
         </ReactScrollLink>
       </motion.div>
       <motion.div
@@ -64,11 +80,18 @@ export default function DesktopMenu(props: { finishedLoading: boolean }) {
         }}
         className="text-AAsecondary"
       >
-        <ReactScrollLink to="SomethingIveBuiltSection" spy={true} smooth={true} offset={-100} duration={200}>
-        &gt; 03. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Work</span>
-
+        <ReactScrollLink
+          to="SomethingIveBuiltSection"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={200}
+        >
+          &gt; 03.{" "}
+          <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">
+            Work
+          </span>
         </ReactScrollLink>
-        
       </motion.div>
       <motion.span
         initial={{
@@ -86,32 +109,53 @@ export default function DesktopMenu(props: { finishedLoading: boolean }) {
         }}
         className="text-AAsecondary"
       >
-         <ReactScrollLink to="GetInTouchSection" spy={true} smooth={true} offset={-100} duration={200}>
-         &gt; 04. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Contact</span>
+        <ReactScrollLink
+          to="GetInTouchSection"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={200}
+        >
+          &gt; 04.{" "}
+          <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">
+            Contact
+          </span>
         </ReactScrollLink>
       </motion.span>
       <a href={"/resume.pdf"} target={"_blank"} rel="noreferrer">
-      <motion.button
-        initial={{
-          y: -40,
-          opacity: 0,
-        }}
-        animate={{
-          y: 0,
-          opacity: 1,
-        }}
-        transition={{
-          type: "spring",
-          duration: props.finishedLoading ? 0 : 1.2,
-          delay: props.finishedLoading ? 0 : 10.2,
-        }}
-        // onClick={()=>{router.push("/resume.pdf")}}
-        className="text-AAsecondary border border-spacing-2 py-2 px-3 rounded-sm border-AAsecondary hover:bg-ResumeButtonHover"
-      >
-        Resume
-      </motion.button>
+        <motion.button data-cursor={true} 
+          initial={{
+            y: -40,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            type: "spring",
+            duration: props.finishedLoading ? 0 : 1.2,
+            delay: props.finishedLoading ? 0 : 10.2,
+          }}
+          // onClick={()=>{router.push("/resume.pdf")}}
+          className="text-AAsecondary border border-spacing-2 py-2 px-3 rounded-sm border-AAsecondary hover:bg-ResumeButtonHover"
+        >
+          Resume
+        </motion.button>
       </a>
-      
+      {/* <div
+        onClick={() => {
+          ReactGA.event({
+            category: "Button.Click",
+            action: "Hire Me",
+          });
+          window.open("mailto:masaudahmod@gmail.com");
+        }}
+        data-cursor={true}
+        className="mb-2 sm:mb-0 whitespace-nowrap text-center text-masaud-dev-green border border-masaud-dev-green rounded  w-full sm:w-auto px-1 text-sm hover:text-masaud-dev-yellow"
+      >
+        Hire me!
+      </div> */}
     </div>
   );
 }
