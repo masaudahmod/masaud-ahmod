b773c7345ae998aaef83a2d7d5e5b1924383e738
masaud04 <ahmedmasaud942@gmail.com>
2025-02-17 22:34:57 +0600

    Update Resume page; display resume PDF in an iframe with improved layout

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,13 @@
-export default function page() {
-  return(
-    <h1>Resume</h1>
-  )
+
+
+export default function Page() {
+  
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <iframe
+        src='../../public/masaud-resume.pdf'
+        className="w-full h-screen border-none"
+      />
+    </div>
+  );
 }
