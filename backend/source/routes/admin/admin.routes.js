0565ba55eb4b386001c463106c1a0f794bd95950
masaudahmod <masaudahmod@gmail.com>
2026-08-18 03:58:07 +0600

    handle and feat admin blog controller about to manage from admin dashboard

--- backend/source/routes/admin/admin.routes.js
+++ backend/source/routes/admin/admin.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authenticate from "../../middlewares/authenticate.js";
 import roleGuard from "../../middlewares/roleGuard.js";
 import * as AdminController from "../../modules/admin/admin.controller.js";
+import rateLimiter from "../../middlewares/rateLimiter.js";
 
 const router = Router();
 
@@ -11,7 +12,7 @@ router.use(authenticate);
 router.get("/admins", roleGuard("SUPER_ADMIN"), AdminController.listAdmins);
 
 // Create admin (only super admin)
-router.post("/admins", roleGuard("SUPER_ADMIN"), AdminController.createAdmin);
+router.post("/admins", roleGuard("SUPER_ADMIN"), rateLimiter({ limit: 5, windowSec: 60 }), AdminController.createAdmin);
 
 // Get single admin (only super admin)
 router.get("/admins/:id", roleGuard("SUPER_ADMIN"), AdminController.getAdmin);
--- backend/source/routes/admin/index.adminRoutes.js
+++ backend/source/routes/admin/index.adminRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import adminRouter from "./admin.routes.js";
+import blogRouter from "./blog.routes.js";
 
 const router = Router();
 
 router.use("/", adminRouter);
+router.use("/blog", blogRouter);
 
 export default router;
